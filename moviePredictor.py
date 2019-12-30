9153a81df9251bd49d2b1a9b6bcc62f61985a4b6
Kenny Gosai <kenny_gosai@hotmail.com>
2019-12-29 21:00:49 -0500

    Fixed comments

--- moviePredictor.py
+++ moviePredictor.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys
 
 if len(sys.argv) != 2:
-    print("Need 1 arguments: python moviePredictor.py <input.txt>")
+    print("Need 1 arguments: python moviePredictor.py <input.csv>")
     sys.exit()
 
 r_cols = ['user_id', 'movie_id', 'rating'] #coloumn names for ratings DataFrame
@@ -13,19 +13,18 @@ movies = pd.read_csv('assets/u.item', sep='|', names=m_cols, usecols=range(2), e
 
 ratings = pd.merge(movies, ratings) #join both tables
 
-userRatings = ratings.pivot_table(index=['user_id'],columns=['title'],values='rating')
+userRatings = ratings.pivot_table(index=['user_id'],columns=['title'],values='rating') # makes column names the title of movies and ratings the values for the corresponding user_id and movie
 
-corrMatrix = userRatings.corr()
+#corrMatrix = userRatings.corr() #gets correlation from every movie pair in userRatings
 
-corrMatrix = userRatings.corr(method='pearson', min_periods=100)
+corrMatrix = userRatings.corr(method='pearson', min_periods=100) #gets correlation from every movie pair in userRatings with a minimum of 100 ratings for each movie.
 
-myRatings = pd.read_csv(sys.argv[1], sep='|', names=['title', 'rating'], usecols=range(2), encoding="ISO-8859-1")
-myRatings.set_index('title', inplace=True)
+myRatings = pd.read_csv(sys.argv[1], sep='|', names=['title', 'rating'], usecols=range(2), encoding="ISO-8859-1") #reads input
+myRatings.set_index('title', inplace=True) #makes title the ID of the dataframe
 
 simCandidates = pd.Series()
 for i in range(0, len(myRatings.index)):
     print ("Adding sims for " + myRatings.index[i] + "...")
-    # print(myRatings['title'][i])
     # Retrieve similar movies to this one that I rated
     sims = corrMatrix[myRatings.index[i]].dropna()
     # sims = corrMatrix[myRatings.index[i]].dropna()
@@ -37,11 +36,9 @@ for i in range(0, len(myRatings.index)):
 #Glance at our results so far:
 print ("sorting...")
 simCandidates.sort_values(inplace = True, ascending = False)
-# print (simCandidates.head(10))
 simCandidates = simCandidates.groupby(simCandidates.index).sum()
 
 simCandidates.sort_values(inplace = True, ascending = False)
-simCandidates.head(10)
 
 filteredSims = simCandidates.drop(myRatings.index)
 print(filteredSims.head(10))
